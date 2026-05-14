66185ab06fe247f63669e8157faa44313bed37da
NomadicMotorhead <128446818+NomadicMotorhead@users.noreply.github.com>
2026-05-14 15:58:43 +0530

    service-worker.js
    No more cache change

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,53 @@
-const CACHE_NAME = 'trackit-pwa-v1';
+const STATIC_CACHE = 'static-assets';
 
-self.addEventListener('install', event => {
+const STATIC_FILES = [
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+    caches.open(STATIC_CACHE)
+      .then(cache => cache.addAll(STATIC_FILES))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== STATIC_CACHE) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+
+  // ALWAYS fetch latest HTML
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  // Cache-first for static assets
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+
 });
